9cfa30bdd387fe46aa57476e54f3d2b4e6b323c6
Shelton-Anotida <rurerure2023@gmail.com>
2023-09-10 11:57:24 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Entry point
+ *
  * puts - function
  *
  * Reurn: 0(Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
