24729c45f00b96dd3d9a9f5f1d00ad3f32200278
Shelton-Anotida <rurerure2023@gmail.com>
2023-09-13 14:47:23 -0700

    X10 X10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,17 @@
 void print_alphabet_x10(void)
 {
 	int No_JAM = 0;
-    
+
 	while (No_JAM < 10)
 	{
 		char JAM = 'a';
-        
+
 		while (JAM <= 'z')
 		{
 			_putchar(JAM);
 			JAM++;
 		}
-        
+
 		_putchar('\n');
 		No_JAM++;
 	}
