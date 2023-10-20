d677129bff082c46a0e794dc22873067c0b42caf
Shelton-Anotida <rurerure2023@gmail.com>
2023-10-20 11:46:10 -0700

    Linked list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: linked list node structure
+ */
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif /* MAIN_H */
+
